79fe5d2ccd6939438842a8057c86920a780b4b41
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-20 10:33:07 -0600

    search form basics added will come back to it after card is made

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) 
+{
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = event =>
+  {
+    setSearchTerm(event.target.value);
+  }
   return (
     <section className="search-form">
-     // Add a search form here
+     <form>
+       <label htmlFor='search'>Search</label>
+       <input id='search' type='text' onChange='handleChange'/>
+     </form>
     </section>
   );
 }
